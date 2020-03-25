d4b7ccecd965cf67cb67d8c84efa21c1d96b10d1
meilixiaozhang <redpangci@gmail.com>
2020-03-25 18:56:08 +1100

    add body-parser

--- calculator.js
+++ calculator.js
@@ -1,8 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-  res.send('Hello you mother World!')
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function(req, res) {
+  var num1 = Number(req.body.n1);
+  var num2 = Number(req.body.n2);
+  var result = num1 + num2;
+  res.send("The result is "+result)
 })
 
 app.listen(3000, function() {
