e81bcf818faeb524291c44b70b3e37fd84db3125
meilixiaozhang <redpangci@gmail.com>
2020-03-25 18:08:10 +1100

    init calculator

--- calculator.js
+++ calculator.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.listen(3000, function(){
-  console.log("Running on port 3000");
+app.get('/', function(req, res) {
+  res.send('Hello you mother World!')
+})
+
+app.listen(3000, function() {
+  console.log("Running on port 3000")
 })
