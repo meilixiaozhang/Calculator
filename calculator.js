746ff2836eb99afb91cf9d5d01f11254e55f2b9f
meilixiaozhang <redpangci@gmail.com>
2020-03-25 19:05:42 +1100

    update bmi calculator

--- calculator.js
+++ calculator.js
@@ -8,6 +8,17 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + "/index.html")
 })
 
+app.get('/bmicalculator', function(req,res){
+  res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/bmicalculator", function(req,res){
+  var weight = Number(req.body.weight);
+  var height = Number(req.body.height);
+  var bmi = weight / (Math.pow(height,2));
+  res.send("Your BMI is "+bmi);
+})
+
 app.post("/", function(req, res) {
   var num1 = Number(req.body.n1);
   var num2 = Number(req.body.n2);
